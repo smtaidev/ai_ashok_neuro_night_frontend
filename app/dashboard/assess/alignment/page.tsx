76ed5313e39458b68f1bc17fc704b7cb3a9c6180
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-08-04 06:28:08 +0600

    added assess page content

---
+++ app/dashboard/assess/alignment/page.tsx
@@ -0,0 +1,8 @@
+export default function AlignmentPage() {
+  return (
+    <div>
+      <h1>Assess  Alignment  Page</h1>
+      <p>This is the content of the Assess Alignment page.</p>
+    </div>
+  );
+}  
