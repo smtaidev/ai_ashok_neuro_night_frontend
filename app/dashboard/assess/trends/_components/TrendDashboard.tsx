60e76dda06b0a92104aff477327f0460b0089530
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-08-10 02:33:05 +0600

    update trend dashboard

--- app/dashboard/assess/trends/_components/TrendDashboard.tsx
+++ app/dashboard/assess/trends/_components/TrendDashboard.tsx
@@ -217,9 +217,9 @@ const TrendsDashboard: React.FC = () => {
             </h2>
             <p className="text-gray-700 leading-relaxed text-sm">
               Lorem Ipsum is simply dummy text of the{' '}
-              <span className="text-blue-600">printing</span> and typesetting industry. Lorem Ipsum has been the industry's 
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a 
-              type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining 
+              <span className="text-blue-600">printing</span> and typesetting industry. Lorem Ipsum has been the industrys
+              standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+              type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
               essentially unchanged. It was popularised in the 1960s with the{' '}
               <span className="text-blue-600">release of Letraset sheets</span> containing Lorem Ipsum 
               passages, and more recently with{' '}
