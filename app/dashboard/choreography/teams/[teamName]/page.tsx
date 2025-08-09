79d9d4c2057a1ac065521b6476d3e5969505518a
Shoyas <md.nasiruddin4067@gmail.com>
2025-08-10 00:40:18 +0600

    feat(nasir-v2): running choreograph team

--- app/dashboard/choreography/teams/[teamName]/page.tsx
+++ app/dashboard/choreography/teams/[teamName]/page.tsx
@@ -1,18 +1,29 @@
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
+// import React from 'react';
+
+// const metadata: Metadata = {
+//   title: 'Asho Kheli',
+//   description: 'Asho Kheli Team Page',
+// };
+
+// const TeamPage = ({ params }: { params: { teamName: string } }) => {
+
+//   return (
+//     <div>
+//       <h1>Asho kheli: {params.teamName}</h1>
+//     </div>
+//   );
+// };
+
+// export default TeamPage;
 import React from 'react';
 
-const metadata: Metadata = {
-  title: 'Asho Kheli',
-  description: 'Asho Kheli Team Page',
-};
-
-const TeamPage = ({ params }: { params: { teamName: string } }) => {
-
+const page = () => {
   return (
     <div>
-      <h1>Asho kheli: {params.teamName}</h1>
+      <h1>Hlw</h1>
     </div>
   );
 };
 
-export default TeamPage;
+export default page;
