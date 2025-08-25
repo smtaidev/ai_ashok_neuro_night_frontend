982efcbb6eaa596bfbb8aacd6cec029d89843625
Shoyas <md.nasiruddin4067@gmail.com>
2025-08-26 00:09:04 +0600

    update

--- app/dashboard/choreography/teams/[teamName]/page.tsx
+++ app/dashboard/choreography/teams/[teamName]/page.tsx
@@ -1,31 +1,31 @@
-// import React from 'react';
-
-// interface TeamPageProps {
-//   params: {
-//     teamName: string;
-//   };
-// }
-
-// const TeamPage = ({ params }: TeamPageProps) => {
-
-//   return (
-//     <div>
-//       <h1>{params.teamName}</h1>
-//       <p>Team Name: {params?.teamName}</p>
-//     </div>
-//   );
-// };
-
-// export default TeamPage;
-
 import React from 'react';
 
-const TeamPage = () => {
+interface TeamPageProps {
+  params: {
+    teamName: string;
+  };
+}
+
+const TeamPage = ({ params }: TeamPageProps) => {
+
   return (
     <div>
-      <h1>This is Team Page</h1>
+      <h1>{params.teamName}</h1>
+      <p>Team Name: {params?.teamName}</p>
     </div>
   );
 };
 
 export default TeamPage;
+
+// import React from 'react';
+
+// const TeamPage = () => {
+//   return (
+//     <div>
+//       <h1>This is Team Page</h1>
+//     </div>
+//   );
+// };
+
+// export default TeamPage;
