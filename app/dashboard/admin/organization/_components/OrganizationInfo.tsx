ef584e73b6f98f3a72ed7f6d1dbaf7e8e6c564c4
Shoyas <md.nasiruddin4067@gmail.com>
2025-09-02 02:15:29 +0600

    feat(nasir): complete organization info

--- app/dashboard/admin/organization/_components/OrganizationInfo.tsx
+++ app/dashboard/admin/organization/_components/OrganizationInfo.tsx
@@ -172,199 +172,7 @@
 
 // export default OrganizationInfo;
 
-
-// "use client"
-
-// import {
-//   useForm
-// } from "react-hook-form"
-// import {
-//   zodResolver
-// } from "@hookform/resolvers/zod"
-// import {
-//   z
-// } from "zod"
-// import {
-//   cn
-// } from "@/lib/utils"
-// import {
-//   Button
-// } from "@/components/ui/button"
-// import {
-//   Form,
-//   FormControl,
-//   FormDescription,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from "@/components/ui/form"
-// import {
-//   Input
-// } from "@/components/ui/input"
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue
-// } from "@/components/ui/select"
-// import {
-//   Textarea
-// } from "@/components/ui/textarea"
-
-// const formSchema = z.object({
-//   founded: z.string().min(1).optional(),
-//   year: z.string().min(1).optional(),
-//   businessType: z.string().optional(),
-//   companyDescriptions: z.string().optional()
-// });
-// import { Card, CardDescription, CardHeader } from '@/components/ui/card';
-// import Image from 'next/image';
-// import React from 'react';
-// import { CardTitle } from '@/components/ui/card';
-// import toast from "react-hot-toast"
-
-// const OrganizationInfo = () => {
-//   const form = useForm<z.infer<typeof formSchema>>({
-//     resolver: zodResolver(formSchema),
-
-//   })
-
-//   function onSubmit(values: z.infer<typeof formSchema>) {
-//     try {
-//       console.log(values);
-//       toast(
-//         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-//           <code className="text-white">{JSON.stringify(values, null, 2)}</code>
-//         </pre>
-//       );
-//     } catch (error) {
-//       console.error("Form submission error", error);
-//       toast.error("Failed to submit the form. Please try again.");
-//     }
-//   }
-//   return (
-//     <div className="dashboard-container">
-//       <div className="p-6 bg-white rounded-md shadow-md border border-gray-300">
-//         <Card className="border border-gray-200 rounded-md">
-//           <CardHeader>
-//             <div className="flex flex-col md:flex-row md:justify-between gap-4 mb-4">
-//               <div className="flex-1 space-y-4 md:text-left">
-//                 <CardTitle className="text-[#0B1C33] text-4xl">
-//                   Organization Information
-//                 </CardTitle>
-//                 <CardDescription className="text-[#000000] mt-1 text-lg">
-//                   Update your organization details
-//                 </CardDescription>
-//               </div>
-//               <div>
-//                 <Image
-//                   src="/image/user-main.png"
-//                   alt="dashboard icon"
-//                   width={900}
-//                   height={900}
-//                   className="rounded-lg w-[570px] h-[316px]"
-//                 />
-//               </div>
-//             </div>
-//             <div className='h-[1px] w-full bg-gray-200' />
-//             <div className="mt-4">
-//               <Form {...form}>
-//                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
-//                   <FormField
-//                     control={form.control}
-//                     name="founded"
-//                     render={({ field }) => (
-//                       <FormItem>
-//                         <FormLabel>Founded</FormLabel>
-//                         <FormControl>
-//                           <Input
-//                             placeholder="Abc"
-
-//                             type=""
-//                             {...field} />
-//                         </FormControl>
-
-//                         <FormMessage />
-//                       </FormItem>
-//                     )}
-//                   />
-
-//                   <FormField
-//                     control={form.control}
-//                     name="year"
-//                     render={({ field }) => (
-//                       <FormItem>
-//                         <FormLabel>Year</FormLabel>
-//                         <FormControl>
-//                           <Input
-//                             placeholder="2024"
-
-//                             type="text"
-//                             {...field} />
-//                         </FormControl>
-
-//                         <FormMessage />
-//                       </FormItem>
-//                     )}
-//                   />
-
-//                   <FormField
-//                     control={form.control}
-//                     name="businessType"
-//                     render={({ field }) => (
-//                       <FormItem>
-//                         <FormLabel>Business Type</FormLabel>
-//                         <Select onValueChange={field.onChange} defaultValue={field.value}>
-//                           <FormControl>
-//                             <SelectTrigger>
-//                               <SelectValue placeholder="See More" />
-//                             </SelectTrigger>
-//                           </FormControl>
-//                           <SelectContent>
-//                             <SelectItem value="m@example.com">m@example.com</SelectItem>
-//                             <SelectItem value="m@google.com">m@google.com</SelectItem>
-//                             <SelectItem value="m@support.com">m@support.com</SelectItem>
-//                           </SelectContent>
-//                         </Select>
-
-//                         <FormMessage />
-//                       </FormItem>
-//                     )}
-//                   />
-
-//                   <FormField
-//                     control={form.control}
-//                     name="companyDescriptions"
-//                     render={({ field }) => (
-//                       <FormItem>
-//                         <FormLabel>Company Description</FormLabel>
-//                         <FormControl>
-//                           <Textarea
-//                             placeholder="Welcome to the....."
-//                             className="resize-none"
-//                             {...field}
-//                           />
-//                         </FormControl>
-
-//                         <FormMessage />
-//                       </FormItem>
-//                     )}
-//                   />
-//                   <Button type="submit">Submit</Button>
-//                 </form>
-//               </Form>
-//             </div>
-//           </CardHeader>
-//         </Card>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default OrganizationInfo;
-
+//! Try - Nasir
 
 "use client"
 
@@ -401,21 +209,24 @@ import {
 import {
   Textarea
 } from "@/components/ui/textarea"
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import Image from 'next/image';
-import React from 'react';
-import toast from "react-hot-toast"
 
 const formSchema = z.object({
+  companyName: z.string().min(1),
   founded: z.string().min(1).optional(),
   year: z.string().min(1).optional(),
   businessType: z.string().optional(),
   companyDescriptions: z.string().optional()
 });
+import { Card, CardDescription, CardHeader } from '@/components/ui/card';
+import Image from 'next/image';
+import React from 'react';
+import { CardTitle } from '@/components/ui/card';
+import toast from "react-hot-toast"
 
 const OrganizationInfo = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+
   })
 
   function onSubmit(values: z.infer<typeof formSchema>) {
@@ -431,108 +242,146 @@ const OrganizationInfo = () => {
       toast.error("Failed to submit the form. Please try again.");
     }
   }
-
   return (
     <div className="dashboard-container">
       <div className="p-6 bg-white rounded-md shadow-md border border-gray-300">
         <Card className="border border-gray-200 rounded-md">
           <CardHeader>
-            <div className="flex flex-col md:flex-row md:justify-between gap-8">
-              {/* Left side: Form */}
-              <div className="flex-1">
-                <CardTitle className="text-[#0B1C33] text-4xl mb-2">
+            <div className="flex flex-col md:flex-row md:justify-between gap-4 mb-4">
+              <div className="flex-1 space-y-4 md:text-left">
+                <CardTitle className="text-[#0B1C33] lg:text-4xl text-2xl">
                   Organization Information
                 </CardTitle>
-                <CardDescription className="text-[#000000] mb-6 text-lg">
+                <CardDescription className="text-[#000000] mt-1 text-lg">
                   Update your organization details
                 </CardDescription>
-
-                <Form {...form}>
-                  <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-xl">
-                    <FormField
-                      control={form.control}
-                      name="founded"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Founded</FormLabel>
-                          <FormControl>
-                            <Input placeholder="Abc" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-
-                    <FormField
-                      control={form.control}
-                      name="year"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Year</FormLabel>
-                          <FormControl>
-                            <Input placeholder="2024" type="text" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-
-                    <FormField
-                      control={form.control}
-                      name="businessType"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Business Type</FormLabel>
-                          <Select onValueChange={field.onChange} defaultValue={field.value}>
-                            <FormControl>
-                              <SelectTrigger>
-                                <SelectValue placeholder="See More" />
-                              </SelectTrigger>
-                            </FormControl>
-                            <SelectContent>
-                              <SelectItem value="m@example.com">m@example.com</SelectItem>
-                              <SelectItem value="m@google.com">m@google.com</SelectItem>
-                              <SelectItem value="m@support.com">m@support.com</SelectItem>
-                            </SelectContent>
-                          </Select>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-
-                    <FormField
-                      control={form.control}
-                      name="companyDescriptions"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Company Description</FormLabel>
-                          <FormControl>
-                            <Textarea
-                              placeholder="Welcome to the....."
-                              className="resize-none"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-
-                    <Button type="submit">Submit</Button>
-                  </form>
-                </Form>
               </div>
-
-              {/* Right side: Image */}
-              <div className="flex-shrink-0">
+              <div>
                 <Image
                   src="/image/user-main.png"
                   alt="dashboard icon"
-                  width={570}
-                  height={316}
-                  className="rounded-lg object-cover"
+                  width={900}
+                  height={900}
+                  className="rounded-lg w-[570px] h-[316px]"
                 />
               </div>
+            </div>
+            <div className='h-[1px] w-full bg-gray-200' />
+            <div className="mt-4 flex justify-start">
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 max-w-6xl py-10">
+
+                  <FormField
+                    control={form.control}
+                    name="companyName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-[18px]">Company Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Company Name"
+                            defaultValue={"Monster, Inc."}
+                            type=""
+                            {...field}
+                            className="lg:w-[640px] h-[48px] md:w-full text-base"
+                            disabled
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="founded"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-[18px]">Founded</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Abc"
+                            type=""
+                            {...field}
+                            className="lg:w-[640px] h-[48px] md:w-full text-base"
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="year"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-[18px]">Year</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="2024"
+                            type="text"
+                            {...field}
+                            className="lg:w-[640px] h-[48px] md:w-full text-base"
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="businessType"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-[18px]">Business Type</FormLabel>
+                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="See More" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent
+                            className="h-[145px] w-[182px] text-base"
+                          >
+                            <SelectItem value="Corporation">Corporation</SelectItem>
+                            <SelectItem value="Partnership">Partnership</SelectItem>
+                            <SelectItem value="Limited Liability">Limited Liability</SelectItem>
+                            <SelectItem value="Sole Proprietorship">Sole Proprietorship</SelectItem>
+                          </SelectContent>
+                        </Select>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="companyDescriptions"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-[18px]">Company Description</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            placeholder="Welcome to the....."
+                            className="h-[233px] w-[1053px] resize-none text-base"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <div className="flex justify-end">
+                    <Button variant={"getStarted"} type="submit">Updated Company Info</Button>
+                  </div>
+                </form>
+              </Form>
             </div>
           </CardHeader>
         </Card>
@@ -542,5 +391,3 @@ const OrganizationInfo = () => {
 };
 
 export default OrganizationInfo;
-
-
